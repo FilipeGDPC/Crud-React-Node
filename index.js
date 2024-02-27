91681384797d6d0ed8f941d18f2e5ce5dc117216
Filipe Gama <filipegama2411@gmail.com>
2024-02-27 12:18:38 -0300

    import userRotes from users.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import Express from "express";
+import userRotes from "./routes/users.js";
 import cors from "cors";
 
 const app = Express();
 
 app.use(Express.json());
 app.use(cors());
+
+app.use("/users", userRotes);
 
 app.listen(8080);
