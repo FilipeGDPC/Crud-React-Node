016566005087cac969e6b7261eb565f4d7e45e4f
Filipe Gama <filipegama2411@gmail.com>
2024-02-28 13:18:29 -0300

    adding user in user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,5 +8,21 @@ export const getUsers = (_, res) => {
 
         return res.status(200).json(data);
     });
-}
-            
+};
+
+export const addUser = (req, res) => {
+    const values =  [
+        req.body.nome,
+        req.body.email,
+        req.body.fone,
+        req.body.data_nasc,
+    ];
+
+    const q = "INSERT INTO usuarios (nome, email, fone, data_nasc) VALUES (?, ?, ?, ?)";
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Usuário cadastrado com sucesso!");
+    });
+};
